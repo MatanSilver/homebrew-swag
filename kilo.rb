81eee2c2855334d8397845bda805faab5dc527ff
MatanSilver <matansilver@gmail.com>
2016-07-26 14:15:51 -0400

    added checksum

--- kilo.rb
+++ kilo.rb
@@ -2,7 +2,7 @@ class Kilo < Formula
   homepage "://github.com/MatanSilver/kilo"
   url "https://github.com/MatanSilver/kilo/archive/1.0.0.tar.gz"
   version "1.0.0"
-  # sha256 ""
+  sha256 "5fd420161fc074fef03ca408f9323e668395ca43c39344b6be24bee023ce3652"
   def install
     system "make"
     bin.install "kilo"
