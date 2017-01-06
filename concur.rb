977f27a94a5eb2cf4b76269745a3f4927d7c55f4
MatanSilver <matansilver@gmail.com>
2017-01-05 22:17:54 -0500

    changed from makefile to go build, might go back eventually

--- concur.rb
+++ concur.rb
@@ -4,7 +4,7 @@ class Concur < Formula
   version "0.1.0"
   sha256 "5d0469b45f8d40521e9ef3296ffb3791e0384ade7893a880f24eda10a91f2547"
   def install
-    system "make all"
+    system "go build"
     bin.install "concur"
   end
 end
