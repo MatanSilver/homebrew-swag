b88503eeff4cf8bfba432ae89ee900f28ab467e5
MatanSilver <matansilver@gmail.com>
2017-01-05 22:16:59 -0500

    fixed url

--- concur.rb
+++ concur.rb
@@ -1,6 +1,6 @@
 class Concur < Formula
   homepage "://github.com/MatanSilver/concur"
-  url "https://github.com/MatanSilver/concur/archive/0.1.0.tar.gz"
+  url "https://github.com/MatanSilver/concur/archive/v0.1.0.tar.gz"
   version "0.1.0"
   sha256 "5d0469b45f8d40521e9ef3296ffb3791e0384ade7893a880f24eda10a91f2547"
   def install
