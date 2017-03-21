0671e4e71c1c04a7691191c438d51235402fc278
MatanSilver <matansilver@gmail.com>
2017-03-21 10:26:06 -0400

    fixed shasum?

--- bish.rb
+++ bish.rb
@@ -1,8 +1,8 @@
 class Bish < Formula
   homepage "://github.com/tdenniston/bish"
   url "https://github.com/tdenniston/bish/archive/v0.1.tar.gz"
-  version "1.0.0"
-  sha256 "1d524bde9ebc58e2164e10562001a1c4022428e54d92e82924c7d32801df1e67"
+  version "0.1.0"
+  sha256 "796d1efcbc9e8c7ea604881860aa8d857aad089c4eeb766283c21c210687942b"
   def install
     system "make"
     bin.install "bish"
