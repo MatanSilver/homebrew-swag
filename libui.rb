22057a8323eabb5e34708e9f1d94d996f3ed9cf4
MatanSilver <matansilver@gmail.com>
2017-07-18 16:31:30 -0400

    separated args

--- libui.rb
+++ libui.rb
@@ -5,15 +5,15 @@ class Libui < Formula
   sha256 "0b5c64363c61f3f679672761979afc824ca87df801c5875297343e079afbc8ba"
   def install
     system "mkdir build"
-    system "cd build"
-    system "cmake .."
+    system "cd" "build"
+    system "cmake" ".."
     system "make"
-    system "cmake -DBUILD_SHARED_LIBS=OFF .."
+    system "cmake " "-DBUILD_SHARED_LIBS=OFF" ".."
     system "make"
-    system "cp out/libui.A.dylib /usr/local/lib/libui.A.dylib"
-    system "cp out/libui.a /usr/local/lib/libui.a"
-    system "cp out/libui.dylib /usr/local/lib/libui.dylib"
-    system "cp ../ui.h /usr/local/include/ui.h"
-    system "cp ../ui_darwin.h /usr/local/include/ui_darwin.h"
+    system "cp" "out/libui.A.dylib" "/usr/local/lib/libui.A.dylib"
+    system "cp" "out/libui.a" "/usr/local/lib/libui.a"
+    system "cp" "out/libui.dylib" "/usr/local/lib/libui.dylib"
+    system "cp" "../ui.h" "/usr/local/include/ui.h"
+    system "cp" "../ui_darwin.h" "/usr/local/include/ui_darwin.h"
   end
 end
