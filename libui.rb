8bfa4ca4985098aa570a768938e363b0bb3d2ba1
MatanSilver <matansilver@gmail.com>
2017-07-27 14:41:25 -0400

    added libui cmake dep?

--- libui.rb
+++ libui.rb
@@ -3,6 +3,9 @@ class Libui < Formula
   url "https://github.com/andlabs/libui/archive/alpha3.1.zip"
   version "0.3.1"
   sha256 "0b5c64363c61f3f679672761979afc824ca87df801c5875297343e079afbc8ba"
+
+  depends_on "cmake" => :build
+
   def install
     system "cmake ."
     system "make"
