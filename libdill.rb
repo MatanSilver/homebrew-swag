1df12c897ef009e7b708d309bf4423e1817fb37a
MatanSilver <matansilver@gmail.com>
2017-07-27 14:10:18 -0400

    try to fix command not found

--- libdill.rb
+++ libdill.rb
@@ -4,8 +4,8 @@ class Libdill < Formula
   version "1.6"
   sha256 "d0a525e5623764a5a4c7458752520316f67985196661a9028a1676ca1db98cc0"
   def install
-    system "./autogen.sh"
-    system "./configure"
+    system "sh", "autogen.sh"
+    system "sh", "configure"
     #system "cmake -DBUILD_SHARED_LIBS=OFF ."
     system "make"
     system "make", "check"
