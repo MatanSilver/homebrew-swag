cb0a52b801a737ab90dcd286439bb2db86d338c9
MatanSilver <matansilver@gmail.com>
2017-07-27 14:21:03 -0400

    try without autogen.sh

--- libdill.rb
+++ libdill.rb
@@ -4,7 +4,7 @@ class Libdill < Formula
   version "1.6"
   sha256 "d0a525e5623764a5a4c7458752520316f67985196661a9028a1676ca1db98cc0"
   def install
-    system "sh", "autogen.sh"
+    #system "sh", "autogen.sh"
     system "sh", "configure"
     #system "cmake -DBUILD_SHARED_LIBS=OFF ."
     system "make"
