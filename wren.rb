375521b41f4497bc14cd58ed8fc4a9be709b8975
MatanSilver <matansilver@gmail.com>
2018-06-22 10:17:15 -0400

    fixed version number

--- wren.rb
+++ wren.rb
@@ -1,7 +1,7 @@
 class Wren < Formula
   homepage "://github.com/munificent/wren"
   url "https://github.com/munificent/wren/archive/0.1.0.tar.gz"
-  version "1.0.0"
+  version "0.1.0"
   sha256 "ebf8687dfdb55997a3fc263d41f306c6f40d9562ccbd945d9c12c48795388eae"
   def install
     system "make", "release"
