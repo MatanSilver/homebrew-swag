55ec2d1f89f5dcc53204c21f26aacd16ed561750
MatanSilver <matansilver@gmail.com>
2018-06-22 10:14:41 -0400

    fixed name

--- wren.rb
+++ wren.rb
@@ -1,8 +1,8 @@
-class Kilo < Formula
-  homepage "://github.com/MatanSilver/kilo"
-  url "https://github.com/MatanSilver/kilo/archive/1.0.0.tar.gz"
+class Wren < Formula
+  homepage "://github.com/munificent/wren"
+  url "https://github.com/munificent/wren/archive/0.1.0.tar.gz"
   version "1.0.0"
-  sha256 "5fd420161fc074fef03ca408f9323e668395ca43c39344b6be24bee023ce3652"
+  sha256 "ebf8687dfdb55997a3fc263d41f306c6f40d9562ccbd945d9c12c48795388eae"
   def install
     system "make" "release"
     bin.install "wren"
